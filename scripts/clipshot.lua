ee78eea897be23de944731ed2d7964d7eb86fc4b
enonibobble <laidbackenon@outlook.com>
2026-07-22 15:40:55 +0100

    restore old right click behaviour, save screenshots to persistent directory instead of temp

--- scripts/clipshot.lua
+++ scripts/clipshot.lua
@@ -3,57 +3,52 @@
 ---@license 0BSD
 
 ---@class ClipshotOptions
----@field name string
 ---@field type string
 local o = {
-    name = 'mpv-screenshot.png',
     type = '' -- defaults to jpeg
 }
 require('mp.options').read_options(o, 'clipshot')
 
-local file, cmd
-
 local platform = mp.get_property_native('platform')
-if platform == 'windows' then
-    file = os.getenv('TEMP')..'\\'..o.name
-    cmd = {
-        'powershell', '-NoProfile', '-Command',
-        'Add-Type -Assembly System.Windows.Forms, System.Drawing;',
-        string.format(
-            "[Windows.Forms.Clipboard]::SetImage([Drawing.Image]::FromFile('%s'))",
-            file:gsub("'", "''")
-        )
-    }
-elseif platform == 'darwin' then
-    file = os.getenv('TMPDIR')..'/'..o.name
-    -- png: «class PNGf»
-    local type = o.type ~= '' and o.type or 'JPEG picture'
-    cmd = {
-        'osascript', '-e', string.format(
-            'set the clipboard to (read (POSIX file %q) as %s)',
-            file, type
-        )
-    }
-else
-    file = '/tmp/'..o.name
-    if os.getenv('XDG_SESSION_TYPE') == 'wayland' then
-        cmd = {'sh', '-c', ('wl-copy < %q'):format(file)}
-    else
-        local type = o.type ~= '' and o.type or 'image/jpeg'
-        cmd = {'xclip', '-sel', 'c', '-t', type, '-i', file}
-    end
-end
 
 ---@param arg string
 ---@return fun()
 local function clipshot(arg)
     return function()
-        mp.commandv('screenshot-to-file', file, arg)
+        local res = mp.command_native({'screenshot', arg})
+        if not res or not res.filename then return end
+        
+        local file = res.filename
+        local cmd
+
+        if platform == 'windows' then
+            cmd = {
+                'powershell', '-NoProfile', '-Command',
+				'Add-Type -Assembly System.Windows.Forms;',
+                string.format(
+                    "$fileList = New-Object System.Collections.Specialized.StringCollection; [void]$fileList.Add('%s'); [Windows.Forms.Clipboard]::SetFileDropList($fileList)",
+                    file:gsub("'", "''")
+                )
+            }
+        elseif platform == 'darwin' then
+            cmd = {
+                'osascript', '-e', string.format(
+                    'set the clipboard to (POSIX file %q)',
+                    file, type
+                )
+            }
+        else
+            if os.getenv('XDG_SESSION_TYPE') == 'wayland' then
+                cmd = {'wl-copy', '--type', 'text/uri-list', string.format('file://%s', file)}
+            end
+        end
+
         mp.command_native_async({'run', unpack(cmd)}, function(suc, _, err)
             mp.osd_message(suc and 'Copied screenshot to clipboard' or err, 1)
         end)
     end
 end
 
-mp.add_key_binding('f16',      'clipshot-subs',   clipshot('subtitles'))
-mp.add_key_binding('shift+f16','clipshot-video',  clipshot('video'))
+mp.add_key_binding('f16',		'clipshot-subs',   clipshot('subtitles'))
+mp.add_key_binding('shift+f16', 'clipshot-video',  clipshot('video'))
+mp.add_key_binding('Alt+f16',	'clipshot-window', clipshot('window'))
